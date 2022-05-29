75e73009edbce6e295bf3672dc8d041dcb4cba53
Sascha1403 <s-steger@live.de>
2022-05-29 13:14:02 +0200

    C++ Tutorial 12 - Operator Overloading, File I/O
    // Binary operators operate on 2 objects
    +, -, *, /, %, ==, !=, >, <, >=, <=, &&, ||,
    !, =, +=, -=, *=, /=, ^, [], &, |
    
    Other Unary Operators : --, *(pointer dereference),
    -> (Member Selection), !, & (Address of), +, -

--- main.cpp
+++ main.cpp
@@ -33,6 +33,10 @@ struct Shape; // Structs are used to model new data types, while // classes mode
 class Circle;
 
 
+// Operator like  + - < > & !
+class Box;
+
+
 
 int main() {
 
@@ -918,5 +922,116 @@ struct Circle : Shape{
     // Override Area()
     double Area(){
         return 3.14159 * std::pow((width / 2), 2);
+    }
+};
+
+// Create a custom Box class with overloaded operators
+class Box{
+public:
+    double length, width, breadth;
+
+    // Used to hold a string representation of a box
+    std::string boxString;
+
+    Box(){
+        length = 1, width = 1, breadth = 1;
+    }
+    Box(double l, double w, double b){
+        length = l, width = w, breadth = b;
+    }
+
+    // You can define customer operators just like
+    // you define functions
+    // This is a unary operator because it operates
+    // on 1 object
+    // Other Unary Operators : --, *(pointer dereference),
+    // -> (Member Selection), !, & (Address of), +, -
+    Box& operator ++ (){
+        length++;
+        width++;
+        breadth++;
+        return *this;
+    }
+
+    // Creates a C string representation which is a
+    // pointer to an array that is null terminated
+    operator const char*() {
+        // Creates a stream that can be loaded with
+        // characters that can then be accessed as
+        // a string object
+        std::ostringstream boxStream;
+        boxStream << "Box : " <<
+                  length << ", " <<
+                  width << ", " <<
+                  breadth;
+
+        // Return a string representation of the stream
+        boxString = boxStream.str();
+
+        // Returns the pointer to the string array
+        return boxString.c_str();
+    }
+
+    // Binary operators operate on 2 objects
+    // +, -, *, /, %, ==, !=, >, <, >=, <=, &&, ||,
+    // !, =, +=, -=, *=, /=, ^, [], &, |
+
+    // Let's add boxes
+    Box operator + (const Box& box2){
+        Box boxSum;
+        boxSum.length = length + box2.length;
+        boxSum.width = width + box2.width;
+        boxSum.breadth = breadth + box2.breadth;
+        return boxSum;
+    }
+
+    // Access items using a subscript operator
+    double operator [] (int x){
+        if(x == 0){
+            return length;
+        } else if(x == 1){
+            return width;
+        } else if(x == 2){
+            return breadth;
+        } else {
+            return 0;
+        }
+    }
+
+    // Check for box equality
+    bool operator == (const Box& box2){
+        return ((length == box2.length) &&
+                (width == box2.width) &&
+                (breadth == box2.breadth));
+    }
+
+    // Check for which is bigger
+    bool operator < (const Box& box2){
+        double thisSize = length + width + breadth;
+        double box2Size = box2.length + box2.width +
+                          box2.breadth;
+        if (thisSize < box2Size){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool operator > (const Box& box2){
+        double thisSize = length + width + breadth;
+        double box2Size = box2.length + box2.width +
+                          box2.breadth;
+        if (thisSize > box2Size){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Overload the assignment operator
+    void operator = (const Box& boxToCopy){
+        length = boxToCopy.length;
+        width = boxToCopy.width;
+        breadth = boxToCopy.breadth;
     }
 };
